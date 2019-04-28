9119848783b6d3e2780e7d00d26ca16735082352
tangxiujuan <1943418084@qq.com>
2019-04-28 18:40:58 +0800

    1

--- src/pages/.umi/LocaleWrapper.jsx
+++ src/pages/.umi/LocaleWrapper.jsx
@@ -1,5 +1,9 @@
-
-import { _setIntlObject, addLocaleData, IntlProvider, intlShape } from 'umi-plugin-locale';
+import {
+  _setIntlObject,
+  addLocaleData,
+  IntlProvider,
+  intlShape
+} from "umi-plugin-locale";
 
 const InjectedWrapper = (() => {
   let sfc = (props, context) => {
@@ -7,86 +11,102 @@ const InjectedWrapper = (() => {
     return props.children;
   };
   sfc.contextTypes = {
-    intl: intlShape,
+    intl: intlShape
   };
   return sfc;
 })();
 
-import 'moment/locale/pt-br';
-import 'moment/locale/zh-cn';
-import 'moment/locale/zh-tw';
+import "moment/locale/pt-br";
+import "moment/locale/zh-cn";
+import "moment/locale/zh-tw";
 
 const baseNavigator = true;
 const useLocalStorage = true;
 
-import { LocaleProvider } from 'antd';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-let defaultAntd = require('antd/lib/locale-provider/zh_CN');
+import { LocaleProvider } from "antd";
+import moment from "moment";
+import "moment/locale/zh-cn";
+let defaultAntd = require("antd/lib/locale-provider/zh_CN");
 defaultAntd = defaultAntd.default || defaultAntd;
 
 const localeInfo = {
-  'en-US': {
+  "en-US": {
     messages: {
-      ...require('E:/workSpace/car_rule_manage_web/src/locales/en-US.js').default,
+      ...require("E:/workSpace/car_rule_manage_web/src/locales/en-US.js")
+        .default
     },
-    locale: 'en-US',
-    antd: require('antd/lib/locale-provider/en_US'),
-    data: require('react-intl/locale-data/en'),
-    momentLocale: '',
+    locale: "en-US",
+    antd: require("antd/lib/locale-provider/en_US"),
+    data: require("react-intl/locale-data/en"),
+    momentLocale: ""
   },
-  'pt-BR': {
+  "pt-BR": {
     messages: {
-      ...require('E:/workSpace/car_rule_manage_web/src/locales/pt-BR.js').default,
+      ...require("E:/workSpace/car_rule_manage_web/src/locales/pt-BR.js")
+        .default
     },
-    locale: 'pt-BR',
-    antd: require('antd/lib/locale-provider/pt_BR'),
-    data: require('react-intl/locale-data/pt'),
-    momentLocale: 'pt-br',
+    locale: "pt-BR",
+    antd: require("antd/lib/locale-provider/pt_BR"),
+    data: require("react-intl/locale-data/pt"),
+    momentLocale: "pt-br"
   },
-  'zh-CN': {
+  "zh-CN": {
     messages: {
-      ...require('E:/workSpace/car_rule_manage_web/src/locales/zh-CN.js').default,
+      ...require("E:/workSpace/car_rule_manage_web/src/locales/zh-CN.js")
+        .default
     },
-    locale: 'zh-CN',
-    antd: require('antd/lib/locale-provider/zh_CN'),
-    data: require('react-intl/locale-data/zh'),
-    momentLocale: 'zh-cn',
+    locale: "zh-CN",
+    antd: require("antd/lib/locale-provider/zh_CN"),
+    data: require("react-intl/locale-data/zh"),
+    momentLocale: "zh-cn"
   },
-  'zh-TW': {
+  "zh-TW": {
     messages: {
-      ...require('E:/workSpace/car_rule_manage_web/src/locales/zh-TW.js').default,
+      ...require("E:/workSpace/car_rule_manage_web/src/locales/zh-TW.js")
+        .default
     },
-    locale: 'zh-TW',
-    antd: require('antd/lib/locale-provider/zh_TW'),
-    data: require('react-intl/locale-data/zh'),
-    momentLocale: 'zh-tw',
-  },
+    locale: "zh-TW",
+    antd: require("antd/lib/locale-provider/zh_TW"),
+    data: require("react-intl/locale-data/zh"),
+    momentLocale: "zh-tw"
+  }
 };
 
 let appLocale = {
-  locale: 'zh-CN',
+  locale: "zh-CN",
   messages: {},
-  data: require('react-intl/locale-data/zh'),
-  momentLocale: 'zh-cn',
+  data: require("react-intl/locale-data/zh"),
+  momentLocale: "zh-cn"
 };
-if (useLocalStorage && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
-  appLocale = localeInfo[localStorage.getItem('umi_locale')];
-} else if (localeInfo[navigator.language] && baseNavigator){
+if (
+  useLocalStorage &&
+  localStorage.getItem("umi_locale") &&
+  localeInfo[localStorage.getItem("umi_locale")]
+) {
+  appLocale = localeInfo[localStorage.getItem("umi_locale")];
+} else if (localeInfo[navigator.language] && baseNavigator) {
   appLocale = localeInfo[navigator.language];
 } else {
-  appLocale = localeInfo['zh-CN'] || appLocale;
+  appLocale = localeInfo["zh-CN"] || appLocale;
 }
 window.g_lang = appLocale.locale;
 appLocale.data && addLocaleData(appLocale.data);
 
 export default function LocaleWrapper(props) {
   let ret = props.children;
-  ret = (<IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
-    <InjectedWrapper>{ret}</InjectedWrapper>
-  </IntlProvider>)
-  ret = (<LocaleProvider locale={appLocale.antd ? (appLocale.antd.default || appLocale.antd) : defaultAntd}>
-    {ret}
-  </LocaleProvider>);
+  ret = (
+    <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+      <InjectedWrapper>{ret}</InjectedWrapper>
+    </IntlProvider>
+  );
+  ret = (
+    <LocaleProvider
+      locale={
+        appLocale.antd ? appLocale.antd.default || appLocale.antd : defaultAntd
+      }
+    >
+      {ret}
+    </LocaleProvider>
+  );
   return ret;
 }
--- src/pages/.umi/initDva.js
+++ src/pages/.umi/initDva.js
@@ -1,11 +1,11 @@
-import dva from 'dva';
-import createLoading from 'dva-loading';
+import dva from "dva";
+import createLoading from "dva-loading";
 
-const runtimeDva = window.g_plugins.mergeConfig('dva');
+const runtimeDva = window.g_plugins.mergeConfig("dva");
 let app = dva({
   history: window.g_history,
-  
-  ...(runtimeDva.config || {}),
+
+  ...(runtimeDva.config || {})
 });
 
 window.g_app = app;
@@ -14,7 +14,19 @@ app.use(createLoading());
   app.use(plugin);
 });
 
-app.model({ namespace: 'global', ...(require('E:/workSpace/car_rule_manage_web/src/models/global.js').default) });
-app.model({ namespace: 'login', ...(require('E:/workSpace/car_rule_manage_web/src/models/login.js').default) });
-app.model({ namespace: 'setting', ...(require('E:/workSpace/car_rule_manage_web/src/models/setting.js').default) });
-app.model({ namespace: 'user', ...(require('E:/workSpace/car_rule_manage_web/src/models/user.js').default) });
+app.model({
+  namespace: "global",
+  ...require("E:/workSpace/car_rule_manage_web/src/models/global.js").default
+});
+app.model({
+  namespace: "login",
+  ...require("E:/workSpace/car_rule_manage_web/src/models/login.js").default
+});
+app.model({
+  namespace: "setting",
+  ...require("E:/workSpace/car_rule_manage_web/src/models/setting.js").default
+});
+app.model({
+  namespace: "user",
+  ...require("E:/workSpace/car_rule_manage_web/src/models/user.js").default
+});
