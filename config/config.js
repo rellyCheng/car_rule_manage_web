c1fca1992abcf3a926ac4b9c53c0616041784d7f
zhangyingjun <707471634@qq.com>
2019-05-05 16:37:30 +0800

    1

--- config/config.js
+++ config/config.js
@@ -1,111 +1,110 @@
 // https://umijs.org/config/
-import os from 'os';
-import pageRoutes from './router.config';
-import webpackPlugin from './plugin.config';
-import defaultSettings from '../src/defaultSettings';
+import os from "os";
+import pageRoutes from "./router.config";
+import webpackPlugin from "./plugin.config";
+import defaultSettings from "../src/defaultSettings";
 
 const plugins = [
   [
-    'umi-plugin-react',
+    "umi-plugin-react",
     {
       antd: true,
       dva: {
-        hmr: true,
+        hmr: true
       },
       targets: {
-        ie: 11,
+        ie: 11
       },
       locale: {
         enable: true, // default false
-        default: 'zh-CN', // default zh-CN
-        baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
+        default: "zh-CN", // default zh-CN
+        baseNavigator: true // default true, when it is true, will use `navigator.language` overwrite default
       },
       dynamicImport: {
-        loadingComponent: './components/PageLoading/index',
+        loadingComponent: "./components/PageLoading/index"
       },
-      ...(!process.env.TEST && os.platform() === 'darwin'
+      ...(!process.env.TEST && os.platform() === "darwin"
         ? {
             dll: {
-              include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
-              exclude: ['@babel/runtime'],
+              include: ["dva", "dva/router", "dva/saga", "dva/fetch"],
+              exclude: ["@babel/runtime"]
             },
-            hardSource: true,
+            hardSource: true
           }
-        : {}),
-    },
-  ],
+        : {})
+    }
+  ]
 ];
 
 // judge add ga
-if (process.env.APP_TYPE === 'site') {
+if (process.env.APP_TYPE === "site") {
   plugins.push([
-    'umi-plugin-ga',
+    "umi-plugin-ga",
     {
-      code: 'UA-72788897-6',
-    },
+      code: "UA-72788897-6"
+    }
   ]);
 }
 export default {
   // add for transfer to umi
   plugins,
   targets: {
-    ie: 11,
+    ie: 11
   },
   define: {
-    APP_TYPE: process.env.APP_TYPE || '',
-    'process.env': {
-      API_ENV: process.env.API_ENV, // 这里是重点吧，获取配置
-    },
-   
+    APP_TYPE: process.env.APP_TYPE || "",
+    "process.env": {
+      API_ENV: process.env.API_ENV // 这里是重点吧，获取配置
+    }
   },
   // 路由配置
   routes: pageRoutes,
   // Theme for antd
   // https://ant.design/docs/react/customize-theme-cn
   theme: {
-    'primary-color': defaultSettings.primaryColor,
+    "primary-color": defaultSettings.primaryColor
   },
   externals: {
-    '@antv/data-set': 'DataSet',
+    "@antv/data-set": "DataSet"
   },
   proxy: {
-    '/api': {
-      target: 'http://localhost:8425/',
-      changeOrigin: true,
-    },
+    "/api": {
+      target: "http://localhost:8425/",
+      changeOrigin: true
+    }
   },
 
-  ignoreMomentLocale: false,//此项为true会造成日期组件中英混合,改为false就中文了
+  ignoreMomentLocale: false, //此项为true会造成日期组件中英混合,改为false就中文了
   lessLoaderOptions: {
-    javascriptEnabled: true,
+    javascriptEnabled: true
   },
   disableRedirectHoist: true,
   cssLoaderOptions: {
     modules: true,
     getLocalIdent: (context, localIdentName, localName) => {
       if (
-        context.resourcePath.includes('node_modules') ||
-        context.resourcePath.includes('ant.design.pro.less') ||
-        context.resourcePath.includes('global.less')
+        context.resourcePath.includes("node_modules") ||
+        context.resourcePath.includes("ant.design.pro.less") ||
+        context.resourcePath.includes("global.less")
       ) {
         return localName;
       }
       const match = context.resourcePath.match(/src(.*)/);
       if (match && match[1]) {
-        const antdProPath = match[1].replace('.less', '');
+        const antdProPath = match[1].replace(".less", "");
         const arr = antdProPath
-          .split('/')
-          .map(a => a.replace(/([A-Z])/g, '-$1'))
+          .split("/")
+          .map(a => a.replace(/([A-Z])/g, "-$1"))
           .map(a => a.toLowerCase());
-        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+        return `antd-pro${arr.join("-")}-${localName}`.replace(/--/g, "-");
       }
       return localName;
-    },
+    }
   },
   manifest: {
-    basePath: '/',
+    basePath: "/"
   },
 
   hash: true,
-  chainWebpack: webpackPlugin,
+  chainWebpack: webpackPlugin
 };
--- src/pages/User/UserManage.js
+++ src/pages/User/UserManage.js
@@ -1,60 +1,68 @@
 import React, { Component } from "react";
 import PageHeaderWrapper from "@/components/PageHeaderWrapper";
-import {Table,Card,Divider,Popconfirm, message,Button, Modal, Form} from 'antd';
-import { connect } from 'dva';
-import router from 'umi/router';
-import UserInfoForm  from './AddUserInfo';
-import UserListFilter from './UserListFilter'
-@connect(({userManage,loading}) => ({
+import {
+  Table,
+  Card,
+  Divider,
+  Popconfirm,
+  message,
+  Button,
+  Modal,
+  Form
+} from "antd";
+import { connect } from "dva";
+import router from "umi/router";
+import UserInfoForm from "./AddUserInfo";
+import UserListFilter from "./UserListFilter";
+@connect(({ userManage, loading }) => ({
   userManage,
-  userListloading:loading.effects['userManage/fetchUserInfo'],
+  userListloading: loading.effects["userManage/fetchUserInfo"]
 }))
 export default class UserManage extends Component {
-
-  state={
-    filterVal:{},
-    current:1
-  }
-  componentDidMount(){
+  state = {
+    filterVal: {},
+    current: 1
+  };
+  componentDidMount() {
     this.fetchList();
   }
 
-  fetchList=(current = this.state.current,values = this.state.filterVal)=>{
+  fetchList = (current = this.state.current, values = this.state.filterVal) => {
     const { dispatch } = this.props;
     values.current = current;
     values.size = 5;
     dispatch({
-      type:'userManage/fetchUserInfo',
-      payload:values
-    })
+      type: "userManage/fetchUserInfo",
+      payload: values
+    });
     this.setState({
       current,
-      filterVal:values
-    })
-  }
-  handleDelUserInfo=(record)=>{
-    const {dispatch} = this.props;
+      filterVal: values
+    });
+  };
+  handleDelUserInfo = record => {
+    const { dispatch } = this.props;
     dispatch({
-      type:'userManage/fetchDelUserInfo',
-      payload:record.id,
-      callback:res=>{
-        if(res.state=="OK"){
-          this.fetchList()
+      type: "userManage/fetchDelUserInfo",
+      payload: record.id,
+      callback: res => {
+        if (res.state == "OK") {
+          this.fetchList();
         }
       }
-    })
-  }
-  addUserInfo=()=>{
+    });
+  };
+  addUserInfo = () => {
     this.setState({
-      openAddUserForm:true
-    })
-  }
-  editorUserInfo=(record)=>{
+      openAddUserForm: true
+    });
+  };
+  editorUserInfo = record => {
     this.setState({
-      openEditUserForm:true,
+      openEditUserForm: true,
       record
-    })
-  }
+    });
+  };
   render() {
     // 用户管理页面/只有拥有admin权限的才可以看到
     // #TODO
@@ -65,68 +73,84 @@ export default class UserManage extends Component {
     // 5.用户详情 弹窗
     // 5-1.用户违章记录
 
-    const columns = [{
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },{
-      title: '性别',
-      dataIndex: 'sex',
-      key: 'sex',
-    },{
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },{
-      title: '用户名',
-      dataIndex: 'userName',
-      key: 'userName',
-    },{
-      title: '驾照分数',
-      dataIndex: 'points',
-      key: 'points',
-    },{
-      title: '操作',
-      key:'actiion',
-      render: (text, record) => (
-        <span>
-          <a href="javascript:;" onClick={()=> this.editorUserInfo(record)}>编辑</a>
+    const columns = [
+      {
+        title: "姓名",
+        dataIndex: "name",
+        key: "name"
+      },
+      {
+        title: "性别",
+        dataIndex: "sex",
+        key: "sex"
+      },
+      {
+        title: "年龄",
+        dataIndex: "age",
+        key: "age"
+      },
+      {
+        title: "用户名",
+        dataIndex: "userName",
+        key: "userName"
+      },
+      {
+        title: "驾照分数",
+        dataIndex: "points",
+        key: "points"
+      },
+      {
+        title: "操作",
+        key: "actiion",
+        render: (text, record) => (
+          <span>
+            <a href="javascript:;" onClick={() => this.editorUserInfo(record)}>
+              编辑
+            </a>
             <Divider type="vertical" />
-            <Popconfirm title="确认删除？" okText="确定" cancelText="取消"  onConfirm={() => this.handleDelUserInfo(record)}>
+            <Popconfirm
+              title="确认删除？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={() => this.handleDelUserInfo(record)}
+            >
               <a href="javascript:;">删除</a>
             </Popconfirm>
-          </span>  
-      ),
-    }];
-    const {userManage,userListloading} = this.props;
-    console.log(userManage)
-     
+          </span>
+        )
+      }
+    ];
+    const { userManage, userListloading } = this.props;
+    console.log(userManage);
+
     return (
       <div>
         <PageHeaderWrapper>
           <Card>
             <Card>
-              <UserListFilter _fetchList = {this.fetchList}/>
-              <Button type="primary" onClick={this.addUserInfo}>添加用户</Button>
+              <UserListFilter _fetchList={this.fetchList} />
+              <Button type="primary" onClick={this.addUserInfo}>
+                添加用户
+              </Button>
             </Card>
-         
-            <Table 
-            dataSource={userManage.userInfo.pageData} 
-            columns={columns} 
-            rowKey="id"
-            loading={userListloading}
-            pagination={{
-              current: userManage.userInfo.pageCurrent,
-              pageSize: userManage.userInfo.pageSize,
-              total: userManage.userInfo.rowCount,
-              showTotal: () => {
-                return `共${userManage.userInfo.rowCount}条`;
-              },
-              showQuickJumper: true,
-              onChange: current => {
-                this.fetchList(current);
-              },
-            }}
+
+            <Table
+              dataSource={userManage.userInfo.pageData}
+              columns={columns}
+              rowKey="id"
+              loading={userListloading}
+              pagination={{
+                current: userManage.userInfo.pageCurrent,
+                pageSize: userManage.userInfo.pageSize,
+                total: userManage.userInfo.rowCount,
+                showTotal: () => {
+                  return `共${userManage.userInfo.rowCount}条`;
+                },
+                showQuickJumper: true,
+                onChange: current => {
+                  this.fetchList(current);
+                }
+              }}
             />
           </Card>
         </PageHeaderWrapper>
@@ -134,31 +158,31 @@ export default class UserManage extends Component {
         <Modal
           title="添加用户"
           visible={this.state.openAddUserForm}
-          onCancel={()=>{
+          onCancel={() => {
             this.setState({
-              openAddUserForm:false
-            })
+              openAddUserForm: false
+            });
           }}
           width={1000}
           footer={null}
           destroyOnClose={true}
         >
-         <UserInfoForm _this={this}/>
+          <UserInfoForm _this={this} />
         </Modal>
 
         <Modal
           title="编辑用户"
           visible={this.state.openEditUserForm}
-          onCancel={()=>{
+          onCancel={() => {
             this.setState({
-              openEditUserForm:false
-            })
+              openEditUserForm: false
+            });
           }}
           width={1000}
           footer={null}
           destroyOnClose={true}
         >
-         <UserInfoForm _this={this} record={this.state.record}/>
+          <UserInfoForm _this={this} record={this.state.record} />
         </Modal>
       </div>
     );
